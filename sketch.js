850e5f1018c7f9da749a66c643935b877e73ca9a
JeffreyYu2018 <jeff.yu831@gmail.com>
2023-04-11 20:57:34 -0400

    Add working cursor and progress bars

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 // Adapted from https://p5js.org/examples/interaction-snake-game.html
 //
-var host = "localhost:4444";
+var host = "cpsc484-03.yale.internal:8888";
 $(document).ready(function() {
   frames.start();
   twod.start();
@@ -34,26 +34,8 @@ var frames = {
     var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
     var left_wrist_z = (frame.people[0].joints[7].position.z - pelvis_z) * -1;
 
-    if (left_wrist_z < 100) {
-      return command;
-    }
-
-    if (left_wrist_x < 200 && left_wrist_x > -200) {
-      if (left_wrist_y > 500) {
-        command = 73; // UP
-      } else if (left_wrist_y < 100) {
-        command = 75; // DOWN
-      }
-    } else if (left_wrist_y < 500 && left_wrist_y > 100) {
-      if (left_wrist_x > 200) {
-        command = 76; // RIGHT
-      } else if (left_wrist_x < -200) {
-        command = 74; // LEFT
-      }
-    }
-
-    cursor_x = left_wrist_x
-    cursor_y = left_wrist_y
+    cursor_x = left_wrist_x + windowWidth/2
+    cursor_y = windowHeight - left_wrist_y
     return command;
   }
 };
@@ -83,15 +65,17 @@ const diff = 10;
 
 let cursor_x = 0
 let cursor_y = 0
-let refreshX, refreshY
+let refreshX, refreshY, redoX, redoY
+let refreshWidth, refreshHeight, redoWidth, redoHeight
 
 let img_top_left,img_top_right,img_bot_left,img_bot_right
-let img_width=350,img_height=250
+let imgWidth=350,imgHeight=250
 let coord
 let counter=0
 let timer=false
 let noises=[],conveniences=[],distances=[]
 let myInterval
+let imgCoord = []
 
 function preload() {
   img_top_left = loadImage('assets/bass.jpg')
@@ -106,11 +90,23 @@ values = [[1,4,3],[2,2,4],[4,1,1],[3,3,2]]
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  coord = [[0,0],[0,windowHeight/2],[windowWidth/2,0],[windowWidth/2,windowHeight/2]] // coordinates of quadrants
+  imgs = [img_top_left,img_top_right,img_bot_left,img_bot_right]
+  // setup image coords
+  for (let i = 0; i < 4; i++) {
+    quadX = coord[i][0]
+    quadY = coord[i][1]
+    imgCoord.push([quadX + windowWidth/32, quadY + windowHeight*3/16])
+  }
+  refreshWidth = 250
+  refreshHeight = 80
+  redoWidth = 250
+  redoHeight = 80
+  vertOffset = 40
 }
 
 function draw() {
-  coord = [[0,0],[0,windowHeight/2],[windowWidth/2,0],[windowWidth/2,windowHeight/2]] // coordinates of quadrants
-  imgs = [img_top_left,img_top_right,img_bot_left,img_bot_right]
+
   background(102);
   let c = color('red');
   // fill(c);
@@ -128,8 +124,8 @@ function draw() {
     c = color('red')
     drawBarCharts(headers, values[i], quadX, quadY)
     drawStudySpaceImages(imgs[i], imgX, imgY)
-    updateProgress(i, imgX, imgY)
   }
+  updateProgress(imgCoord)
 
   // Your Matches
   c = color('#D3D3D3')
@@ -146,91 +142,70 @@ function draw() {
   // Refresh
   c = color('green')
   fill(c);
-  RefreshWidth = 250
-  RefreshHeight = 80
-  VertOffset = 40
+
   refreshX = windowWidth*3/4
-  refreshY = VertOffset
-  rect(refreshX, refreshY, RefreshWidth, RefreshHeight, 20)
+  refreshY = vertOffset
+  rect(refreshX, refreshY, refreshWidth, refreshHeight, 20)
   textAlign(CENTER);
   textSize(30);
   c = color('black')
   fill(c)
-  text('Refresh', windowWidth*3/4+RefreshWidth/2, RefreshHeight/2+VertOffset)
+  text('Refresh', windowWidth*3/4+refreshWidth/2, refreshHeight/2+vertOffset)
 
   // Redo Quiz 
+  redoX = windowWidth-(windowWidth*3/4)-redoWidth
+  redoY = vertOffset
   c = color('pink')
   fill(c);
-  RedoWidth = 250
-  RedoHeight = 80
-  VertOffset = 40
-  rect(windowWidth-(windowWidth*3/4)-RedoWidth, VertOffset, RedoWidth, RedoHeight, 20)
+  rect(redoX, redoY, redoWidth, redoHeight, 20)
   textAlign(CENTER);
   textSize(30);
   c = color('black')
   fill(c)
-  text('Redo Quiz', windowWidth/4-RedoWidth/2, RedoHeight/2+VertOffset)
+  text('Redo Quiz', windowWidth/4-redoWidth/2, redoHeight/2+vertOffset)
 
 
   stroke(255);
   c = color('red')
   fill(c);
-  console.log("counter equals: " + counter)
   // WITH MOUSE
-  arc(mouseX, mouseY, 80, 80, 0, (counter / 5) * QUARTER_PI);
-  c = color('blue')
+  arc(cursor_x, cursor_y, 80, 80, 0, (counter / 5) * QUARTER_PI);
+  c = color('black')
   fill(c)
-  ellipse(mouseX, mouseY, 50, 50);
+  ellipse(cursor_x, cursor_y, 50, 50)
   // img_top_left.mouseOver(changeGray)
-  updateRefresh()
 
 
   // DRAWING COMMAND WITH KINECT DATA
-  // ellipse(cursor_x+width/2, cursor_y+height/2, 30, 30)
+  
 }
 
 function drawStudySpaceImages(img, imgX, imgY) {
-  image(img, imgX, imgY, img_width, img_height);
+  image(img, imgX, imgY, imgWidth, imgHeight);
 }
 
-function updateProgress(id, imgX, imgY) {
-  if (mouseX > imgX && mouseX < (imgX + img_width)) {
-    if (mouseY > imgY && mouseY < (imgY + img_height)){
-      switch (id) {
-        case 0:
-          console.log("IMAGE ONE")
-          break;
-        case 1:
-          console.log("IMAGE TWO")
-          break;
-        case 2:
-          console.log("IMAGE THREE")
-          break;
-        case 3:
-          console.log("IMAGE FOUR")
-          break;
-      } 
-      if (!timer) {
-        myInterval = setInterval(function () {
-          counter++;
-        }, 50);
-        timer = true
-      }
-    } 
-    else {
-      // clearInterval(myInterval)
+function updateProgress(imgCoord) {
+  let imgX_0 = imgCoord[0][0], imgX_1 = imgCoord[1][0], imgX_2 = imgCoord[2][0], imgX_3 = imgCoord[3][0]
+  let imgY_0 = imgCoord[0][1], imgY_1 = imgCoord[1][1], imgY_2 = imgCoord[2][1], imgY_3 = imgCoord[3][1]
+
+  if ((cursor_x > imgX_0 && cursor_x < (imgX_0 + imgWidth) && cursor_y > imgY_0 && cursor_y < (imgY_0 + imgHeight)) ||
+    (cursor_x > imgX_1 && cursor_x < (imgX_1 + imgWidth) && cursor_y > imgY_1 && cursor_y < (imgY_1 + imgHeight)) ||
+    (cursor_x > imgX_2 && cursor_x < (imgX_2 + imgWidth) && cursor_y > imgY_2 && cursor_y < (imgY_2 + imgHeight)) ||
+    (cursor_x > imgX_3 && cursor_x < (imgX_3 + imgWidth) && cursor_y > imgY_3 && cursor_y < (imgY_3 + imgHeight)) ||
+    (cursor_x > refreshX && cursor_x < (refreshX + refreshWidth) && cursor_y > refreshY && cursor_y < (refreshY + refreshHeight)) ||
+    (cursor_x > redoX && cursor_x < (redoX + redoWidth) && cursor_y > redoY && cursor_y < (redoY + redoHeight))
+  ) {
+    if (!timer) {
+      myInterval = setInterval(function () {
+        counter++;
+      }, 50);
+      timer = true
     }
   } else {
-    // clearInterval(myInterval)
-  }
-}
-
-function updateRefresh() {
-  if (mouseX > refreshX && mouseX < (refreshX + refreshWidth)) {
-    if (mouseY > refreshY && mouseY < (refreshY + refreshHeight)){
-      console.log("REFRESH")
-    } 
-  }
+    clearInterval(myInterval)
+    timer = false
+    counter = 0
+  } 
 }
 
 function drawBarCharts(headers, values, originX, originY) {
@@ -251,30 +226,3 @@ function drawBarCharts(headers, values, originX, originY) {
     rect(originX + windowWidth*3/8, originY + windowHeight / 4 + i * 30, barMaxLength - values[i] * 50, 30)
   }
 }
-
-function sendWristCommand(command) {
-  switch (command) {
-    case 74:
-      if (direction !== 'right') {
-        direction = 'left';
-      }
-      break;
-    case 76:
-      if (direction !== 'left') {
-        direction = 'right';
-      }
-      break;
-    case 73:
-      if (direction !== 'down') {
-        direction = 'up';
-      }
-      break;
-    case 75:
-      if (direction !== 'up') {
-        direction = 'down';
-      }
-      break;
-  }
-  console.log(direction);
-}
-
