6096b4fd95f6d23f34bc0a6d548a74230f0291af
Emmanuel Wilford <75589095+Wilfordabird@users.noreply.github.com>
2023-04-22 13:47:14 -0400

    Update main.js

--- A5/main.js
+++ A5/main.js
@@ -2,7 +2,7 @@
 const address = "15+High+St,+New+Haven,+CT";
 
 // Replace the YOUR_API_KEY with your actual Google Maps Embed API key
-const apiKey = "AIzaSyC3S3ncQh-Zrn3Cln5pdunkKoUECoAqvA0";
+const apiKey = YOUR_API_KEY;
 
 // Construct the Google Maps Embed URL
 const mapUrl = `https://www.google.com/maps/embed/v1/search?key=${apiKey}&q=${address}`;
