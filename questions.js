3bcb36b2f204b1ec0cc27da8ede68580ea203ff7
JeffreyYu2018 <jeff.yu831@gmail.com>
2023-04-24 13:33:01 -0400

    Implement cursor functionality on welcome and questions page

--- questions.js
+++ questions.js
@@ -145,3 +145,179 @@ choiceD.addEventListener('click', () => {
     changeQuestion(1);
 
 });
+
+
+// Jeff's CODE BELOW
+// Adapted from https://p5js.org/examples/interaction-snake-game.html
+// Code for connecting to Kinect
+var host = "cpsc484-03.yale.internal:8888";
+// var host = "localhost:4444";
+$(document).ready(function() {
+  frames.start();
+  twod.start();
+});
+
+var frames = {
+  socket: null,
+
+  start: function() {
+    var url = "ws://" + host + "/frames";
+    frames.socket = new WebSocket(url);
+    frames.socket.onmessage = function (event) {
+      frames.get_left_wrist_command(JSON.parse(event.data));
+    }
+  },
+
+  get_left_wrist_command: function (frame) {
+    if (frame.people === undefined || frame.people.length == 0) {
+      console.log("no people in frame")
+    } else {
+      var pelvis_x = frame.people[0].joints[0].position.x;
+      var pelvis_y = frame.people[0].joints[0].position.y;
+      var left_wrist_x = (frame.people[0].joints[7].position.x - pelvis_x) * -1;
+      var left_wrist_y = (frame.people[0].joints[7].position.y - pelvis_y) * -1;
+  
+      cursor_x = (1.5*left_wrist_x) + windowWidth/2
+      cursor_y = windowHeight - (1.5*left_wrist_y)
+    }
+  }
+};
+
+var twod = {
+  socket: null,
+
+  start: function() {
+    var url = "ws://" + host + "/twod";
+    twod.socket = new WebSocket(url);
+    twod.socket.onmessage = function(event) {
+      twod.show(JSON.parse(event.data));
+    }
+  },
+
+  show: function(twod) {
+    $('.twod').attr("src", 'data:image/pnjpegg;base64,'+twod.src);
+  }
+};
+
+// DECLARE VARIABLES
+// let mouseX = 0, mouseY = 0  // cursor tracks the left wrist of the person on the Kinect
+
+// settings of the start button
+let prev_x, prev_y, prev_width, prev_height
+let nav_x, nav_y, nav_width, nav_height
+let bA_x, bA_y, bB_x, bB_y, bC_x, bC_y, bD_x, bD_y
+let bA_width, bA_height, bB_width, bB_height, bC_width, bC_height, bD_width, bD_height
+
+// settings of the timers and progress management 
+let counter = 0 // keeps track of the progress bar
+let timer = false // is the cursor hovering over an area that can be selected?
+let myInterval  // function for updating the progress arc
+
+function setup() {
+  var myCanvas = createCanvas(window.innerWidth, window.innerHeight);
+  myCanvas.parent("container");
+
+  buttonA = select('#choiceA')
+  buttonB = select('#choiceB')
+  buttonC = select('#choiceC')
+  buttonD = select('#choiceD')
+  prev = select('#prevBtn')
+  nav = select('#navbar')
+
+  bA_x = buttonA.position().x
+  bA_y = buttonA.position().y
+  bB_x = buttonB.position().x
+  bB_y = buttonB.position().y
+  bC_x = buttonC.position().x
+  bC_y = buttonC.position().y
+  bD_x = buttonD.position().x
+  bD_y = buttonD.position().y
+  bA_width = buttonA.width
+  bA_height = buttonA.height
+  bB_width = buttonB.width
+  bB_height = buttonB.height
+  bC_width = buttonC.width
+  bC_height = buttonC.height
+  bD_width = buttonD.width
+  bD_height = buttonD.height
+  prev_x = prev.position().x
+  prev_y = prev.position().y
+  prev_width = prev.width
+  prev_height = prev.height
+  nav_x = nav.position().x
+  nav_y = nav.position().y
+  nav_width = nav.width
+  nav_height = nav.height
+
+  console.log(prev_x, prev_y, prev_width, prev_height)
+}
+
+function draw() {
+  clear()
+  // update cursor accordingly
+  updateProgress()
+
+  // Draw progress cursor
+  stroke(255);
+  c = color('red')
+  fill(c);
+  arc(mouseX, mouseY, 80, 80, 0, (counter / 5) * QUARTER_PI);
+  c = color('black')
+  fill(c)
+  ellipse(mouseX, mouseY, 50, 50)
+}
+
+function updateProgress() {
+  // Navigate amongst the page based on status of progress counter
+  if (counter > 40) {
+    // Check where the cursor is pointed
+    if ((mouseX > bA_x) && (mouseX < (bA_x + bA_width)) && (mouseY > bA_y + nav_height) && (mouseY < (bA_y + bA_height + nav_height))) {
+      console.log(questions[currentQuestion].choices[0]);
+      answers.push(1);
+      changeQuestion(1);
+    } else if  ((mouseX > bB_x) && (mouseX < (bB_x + bB_width)) && (mouseY > bB_y + nav_height) && (mouseY < (bB_y + bB_height + nav_height))) {
+      // save choice
+      console.log(questions[currentQuestion].choices[1]);
+      answers.push(2);
+      changeQuestion(1);
+    } else if ((mouseX > bC_x) && (mouseX < (bC_x + bC_width)) && (mouseY > bC_y + nav_height) && (mouseY < (bC_y + bC_height + nav_height))) {
+      // save choice
+      console.log(questions[currentQuestion].choices[2]);
+      answers.push(3);
+      changeQuestion(1);
+    } else if ((mouseX > bD_x) && (mouseX < (bD_x + bD_width)) && (mouseY > bD_y + nav_height) && (mouseY < (bD_y + bD_height + nav_height))) {
+      // save choice
+      console.log(questions[currentQuestion].choices[3]);
+      answers.push(4);
+      changeQuestion(1);
+    } else if ((mouseX > prev_x) && (mouseX < (prev_x + prev_width)) && (mouseY > prev_y) && (mouseY < (prev_y + prev_height))) {
+      changeQuestion(-1);
+      // remove the last answer
+      answers.pop();
+    }
+    // Cleanup
+    clearInterval(myInterval)
+    timer = false
+    counter = 0
+    // Cleanup
+    clearInterval(myInterval)
+    timer = false
+    counter = 0
+  } else if (((mouseX > bA_x) && (mouseX < (bA_x + bA_width)) && (mouseY > bA_y + nav_height) && (mouseY < (bA_y + bA_height + nav_height))) ||
+    ((mouseX > bB_x) && (mouseX < (bB_x + bB_width)) && (mouseY > bB_y + nav_height) && (mouseY < (bB_y + bB_height + nav_height))) ||
+    ((mouseX > bC_x) && (mouseX < (bC_x + bC_width)) && (mouseY > bC_y + nav_height) && (mouseY < (bC_y + bC_height + nav_height))) ||
+    ((mouseX > bD_x) && (mouseX < (bD_x + bD_width)) && (mouseY > bD_y + nav_height) && (mouseY < (bD_y + bD_height + nav_height))) ||
+    ((mouseX > prev_x) && (mouseX < (prev_x + prev_width)) && (mouseY > prev_y) && (mouseY < (prev_y + prev_height)))
+  ) {
+    if (!timer) {
+      myInterval = setInterval(function () {
+        counter++;
+      }, 50);
+      timer = true
+    } 
+  } else {
+    clearInterval(myInterval)
+    timer = false
+    counter = 0
+  }
+}
