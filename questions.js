95a6befa3f493b4e4eb585cbf6b8b1a42cab89a0
JeffreyYu2018 <jeff.yu831@gmail.com>
2023-04-25 14:04:37 -0400

    Update cursor functionality across as js file

--- questions.js
+++ questions.js
@@ -195,8 +195,8 @@ var frames = {
         var left_wrist_x = (frame.people[body_id_index].joints[7].position.x - pelvis_x) * -1;
         var left_wrist_y = (frame.people[body_id_index].joints[7].position.y - pelvis_y) * -1;
   
-        cursor_x = (1.5*left_wrist_x) + windowWidth/2
-        cursor_y = windowHeight - (1.5*left_wrist_y)
+        cursor_x = (((1.5*left_wrist_x) + windowWidth/2) + cursor_x)/2
+        cursor_y = ((windowHeight - (1.5*left_wrist_y)) + cursor_y) / 2
 
       } else {
         // check if the body_id is still there
@@ -206,6 +206,7 @@ var frames = {
           if (frame.people[b].body_id === body_id) {
             body_id = frame.people[b].body_id
             body_id_index = b
+            break;
           }
         }
         // is the person still there? if not, reset the loop and exist
@@ -217,8 +218,8 @@ var frames = {
           var left_wrist_x = (frame.people[body_id_index].joints[7].position.x - pelvis_x) * -1;
           var left_wrist_y = (frame.people[body_id_index].joints[7].position.y - pelvis_y) * -1;
 
-          cursor_x = (1.5*left_wrist_x) + windowWidth/2
-          cursor_y = windowHeight - (1.5*left_wrist_y)
+          cursor_x = (((1.5*left_wrist_x) + windowWidth/2) + cursor_x)/2
+          cursor_y = ((windowHeight - (1.5*left_wrist_y)) + cursor_y) / 2
         }
       }
     }
